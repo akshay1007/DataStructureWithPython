05334d3d18b9cc26520908ddf75363994a4deec7
akshay jain <akshay.jain@mfine.co>
2021-09-27 14:28:09 +0530

    Linear Search with test cases

---
+++ LinearSearch.py
@@ -0,0 +1,90 @@
+def locate_card(card, query):
+    # Check card is present at starting occurrences
+    occurrences = 0
+    # Set up a loop for repetition
+    while occurrences < len(card):
+        # when card is found
+        if card[occurrences] == query:
+            return occurrences
+
+        # Increase the occurrences
+        occurrences += 1
+
+        # Check if we reached the end of the array
+        if occurrences == len(card):
+            return -1
+
+    return -1
+
+
+def locate_card2(cards, query):
+    # Create a variable position with the value 0
+    position = 0
+
+    # Set up a loop for repetition
+    while True:
+
+        # Check if element at the current position matche the query
+        if cards[position] == query:
+            # Answer found! Return and exit..
+            return position
+
+        # Increment the position
+        position += 1
+
+        # Check if we have reached the end of the array
+        if position == len(cards):
+            # Number not found, return -1
+            return -1
+
+
+if __name__ == '__main__':
+    # Program starting point
+    tests = [{
+        # card found at first place
+        'input': {
+            'card': [10, 20, 30, 40, 50],
+            'query': 10
+        },
+        'output': 0
+    }, {
+        # card should found at 6th place
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0],
+            'query': 1
+        },
+        'output': 6
+    }, {
+        # function should return -1 not found
+        'input': {
+            'card': [],
+            'query': 10
+        },
+        'output': -1
+
+    }, {
+        # Element is not present
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0],
+            'query': 55
+        },
+        'output': -1
+
+    },
+        {
+            # Duplicate element in cards
+            'input': {
+                'card': [13, 11, 10, 7, 4, 3, 1, 0, 7, 4, 3, 1, 0],
+                'query': 7
+            },
+            'output': 3
+
+        }
+    ]
+    for ele in tests:
+        print('Test Case output  : ', locate_card(ele.get("input").get("card"), ele.get("input").get("query")),
+              'Test Case Expected Output : ', ele.get("output"),
+              'Result : ', ele.get("output")==locate_card(ele.get("input").get("card"), ele.get("input").get("query"))
+
+
+              )
--- main.py
+++ main.py
@@ -3,14 +3,118 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from pprint import pprint
+from jovian.pythondsa import evaluate_test_cases
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def locate_card(card, query):
+    # create a variable position with the value 0
+    position = 0
+
+    # Set up a loop for repetition
+    while True:
+        print('position: ', position)
+        # Check if element at the current position match the query
+        if card[position] == query:
+            # Answer found! Return and exit
+            return position
+
+        # Increase the position
+        position += 1
+
+        # Check if we reached the end of the array
+        if position == len(card):
+            return -1
+
+        # Number not found, return -1
+        return -1
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    card = [13, 11, 10, 7, 4, 3, 1, 0]
+    query = 7
+    output = 3
+    tests = []
+    # query for random number
+    test = {
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0],
+            'query': 7
+        },
+        'output': 3
+    }
+    tests.append(test)
+    # query appear in the middle
+    tests.append({
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0],
+            'query': 1
+        },
+        'output': 6
+    })
+
+    # query appear in the first place
+    tests.append({
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0],
+            'query': 13
+        },
+        'output': 0
+    })
+
+    # query appear in the last
+    tests.append({
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0, -127],
+            'query': 0
+        },
+        'output': 7
+    })
+    # card does not contain query
+    tests.append({
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0, -127],
+            'query': 88
+        },
+        'output': -1
+    })
+
+    # card array is empty
+    tests.append({
+        'input': {
+            'card': [],
+            'query': 88
+        },
+        'output': -1
+    })
+
+    # numbers can repeat in the cards
+    tests.append({
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 0, -127, 10, 7, 4, 3, 1, 0, -127, 10, 10, 7, 4, 3, 1, 0, -127],
+            'query': 10
+        },
+        'output': 2
+    })
+
+    # numbers can repeat in the cards
+    tests.append({
+        'input': {
+            'card': [13, 11, 10, 7, 4, 3, 1, 55, 0, -127, 10, 7, 4, 3, 1, 0, -127, 10, 10, 7, 4, 3, 1, 0, -127],
+            'query': 55
+        },
+        'output': 7
+    })
+
+    for test in tests:
+        evaluate_test_cases(locate_card, tests)
+
+# print(locate_card(**test['input']) == test['output'])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
